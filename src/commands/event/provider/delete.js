846decbe9692589a9b7d821889431787605ebd46
Sangeetha Krishnan <sangeetha5491@gmail.com>
2020-06-08 18:42:57 +0530

    Added Provider Commands (#4)
    * fix: use codecov bash uploader
    
    This works in Travis CI windows because the build is run under git-bash.
    
    * fix: remove node 8 support
    
    * chore(readme): remove Greenkeeper badge (#1)
    
    Co-authored-by: greenkeeper[bot] <23040076+greenkeeper[bot]@users.noreply.github.com>
    Co-authored-by: Shazron Abdullah <shaz@adobe.com>
    
    * added provider commands
    
    * fixing review comments
    
    * confirm befre delete
    
    * removing \ in regex and spelling errors
    
    * modify log statement
    
    Co-authored-by: Shazron Abdullah <shazron@gmail.com>
    Co-authored-by: greenkeeper[bot] <23040076+greenkeeper[bot]@users.noreply.github.com>
    Co-authored-by: Shazron Abdullah <shaz@adobe.com>

---
+++ src/commands/event/provider/delete.js
@@ -0,0 +1,57 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const BaseCommand = require('../../../BaseCommand.js')
+const { cli } = require('cli-ux')
+const inquirer = require('inquirer')
+const aioLogger = require('@adobe/aio-lib-core-logging')('@adobe/aio-cli-plugin-events:provider:get', { provider: 'debug' })
+
+class ProviderDeleteCommand extends BaseCommand {
+  async run () {
+    const { args } = this.parse(ProviderDeleteCommand)
+
+    try {
+      await this.initSdk()
+      const response = await inquirer.prompt([{
+        type: 'confirm',
+        name: 'delete',
+        message: 'Are you sure you want to delete the provider? This operation in irreversible.'
+
+      }])
+      if (response.delete) {
+        cli.action.start('Deleting Event Provider')
+        await this.eventClient.deleteProvider(this.conf.org.id,
+          this.conf.project.id, this.conf.workspace.id, args.providerId)
+        cli.action.stop()
+        this.log('Provider ' + args.providerId +
+                ' has been deleted successfully')
+      } else {
+        this.log('Delete operation has been cancelled')
+      }
+    } catch (err) {
+      aioLogger.debug(err)
+      this.error(err)
+    }
+  }
+}
+
+ProviderDeleteCommand.description = 'Delete Provider by id'
+
+ProviderDeleteCommand.args = [
+  { name: 'providerId', required: true }
+]
+
+ProviderDeleteCommand.flags = {
+  ...BaseCommand.flag
+}
+
+module.exports = ProviderDeleteCommand
